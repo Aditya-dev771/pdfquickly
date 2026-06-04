79379f76d9cb2e075fac89b4e2d19acfcd15004b
Aditya-dev771 <dtraushan@gmail.com>
2026-06-04 17:55:46 +0530

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -79,6 +80,19 @@ export default function RootLayout({
   lang="en"
   className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 >
+<Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-9X1SGZJR7R"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-9X1SGZJR7R');
+  `}
+</Script>
   <body className="min-h-full bg-slate-50 text-slate-950 flex flex-col">
     {children}
   </body>
